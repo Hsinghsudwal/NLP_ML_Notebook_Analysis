45abd3eea6fe7f35d971fce2fb2bbd048e532a61
Harinder Singh Sudwal <56237428+Hsinghsudwal@users.noreply.github.com>
2025-03-05 14:08:36 -0800

    notes nlp

--- app.py
+++ app.py
@@ -1,106 +1,239 @@
-
-import streamlit as st  
-from textblob import TextBlob
+import streamlit as st
 import pandas as pd
-import altair as alt
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-
-
-# Fxn
-def convert_to_df(sentiment):
-	sentiment_dict = {'polarity':sentiment.polarity,'subjectivity':sentiment.subjectivity}
-	sentiment_df = pd.DataFrame(sentiment_dict.items(),columns=['metric','value'])
-	return sentiment_df
-
-def analyze_token_sentiment(docx):
-	analyzer = SentimentIntensityAnalyzer()
-	pos_list = []
-	neg_list = []
-	neu_list = []
-	for i in docx.split():
-		res = analyzer.polarity_scores(i)['compound']
-		if res > 0.1:
-			pos_list.append(i)
-			pos_list.append(res)
-
-		elif res <= -0.1:
-			neg_list.append(i)
-			neg_list.append(res)
-		else:
-			neu_list.append(i)
-
-	result = {'positives':pos_list,'negatives':neg_list,'neutral':neu_list}
-	return result 
-
-
-
-
-		
+import numpy as np
+from textblob import TextBlob
+import seaborn as sns
+from nlp import NLPNaive
 
 
 
 
 
+# Create space betwwen two context
+def space():
+    st.markdown("<br>", unsafe_allow_html=True)
 
-def main():
-	st.title("Sentiment Analysis NLP App")
-	st.subheader("Streamlit Projects")
 
-	menu = ["Home","About"]
-	choice = st.sidebar.selectbox("Menu",menu)
-
-	if choice == "Home":
-		st.subheader("Home")
-		with st.form(key='nlpForm'):
-			raw_text = st.text_area("Enter Text Here")
-			submit_button = st.form_submit_button(label='Analyze')
-
-		# layout
-		col1,col2 = st.columns(2)
-		if submit_button:
-
-			with col1:
-				st.info("Results")
-				sentiment = TextBlob(raw_text).sentiment
-				st.write(sentiment)
-
-				# Emoji
-				if sentiment.polarity > 0:
-					st.markdown("Sentiment:: Positive :smiley: ")
-				elif sentiment.polarity < 0:
-					st.markdown("Sentiment:: Negative :angry: ")
-				else:
-					st.markdown("Sentiment:: Neutral 😐 ")
-
-				# Dataframe
-				result_df = convert_to_df(sentiment)
-				st.dataframe(result_df)
-
-				# Visualization
-				c = alt.Chart(result_df).mark_bar().encode(
-					x='metric',
-					y='value',
-					color='metric')
-				st.altair_chart(c,use_container_width=True)
+# Heading
+st.markdown("<h1 style='text-align: center; color: #3f3f44'>NLP - Naive bayes</h1>", unsafe_allow_html=True)
+space()
+# Sub-Heading
+st.markdown("<strong><p style='color: #424874'>1) This project uses Naive Bayes Algorithm</p></strong>", unsafe_allow_html=True)
+st.markdown("<strong><p style='color: #424874'>2) You can choose different cleaning process (Stemming, Lemmatizing)</p></strong>", unsafe_allow_html=True)
+st.markdown("<strong><p style='color: #424874'>3) Different type of  Metrics formation (Count Vectorizing, TF-IDF)</p></strong>", unsafe_allow_html=True)
+st.markdown("<strong><p style='color: #424874'>4) Plotting Sentimental Analysis, Confusion Metrics and Word Cloud</p></strong>", unsafe_allow_html=True)
+space()
 
 
 
-			with col2:
-				st.info("Token Sentiment")
+# Preprocessing data
+def preprocess():
+    try:
+        preprocessing_option = ["Stemming", "Lemmatizing"]
+        preprocessor = st.selectbox("Select Preprocessing Technique", preprocessing_option)
+        return preprocessor
+ 
+    except Exception as e:
+        print("preprocess ERROR : ", e)
 
-				token_sentiments = analyze_token_sentiment(raw_text)
-				st.write(token_sentiments)
+# hyperparameters
+def hyperparameter():
+    try:
+        features = ["2500", "3000", "3500", "4000"]
+        max_features = st.selectbox("Maximum Features you want to restrict?", features)
+        space()
+        space()
+        ranges = ["1,1", "1,2", "1,3"]
+        ngram_range = st.selectbox("Combination of words", ranges).split(',')
+        return max_features, ngram_range
+
+    except Exception as e:
+        print("hyperparameter ERROR : ", e)
 
 
 
 
+# Creating Bag of Words
+def boW():
+    try:
+        metrics = ["count_vectorizing", "tfidf"]
+        bag_of_words = st.selectbox("Bag of Word Technique?", metrics)
+        return bag_of_words
+
+    except Exception as e:
+        print("boW ERROR : ", e)
+
+# Converting Target categorical variable into Numerical Variable
+def y_label():
+    try:
+        target_variable = ["Yes", "No"]
+        y_option = st.selectbox("Do you want to One Hot Encode Target Variable?", target_variable)
+        return y_option
+
+    except Exception as e:
+        print("y_label ERROR : ", e)
 
 
-	else:
-		st.subheader("About")
+# Main function
+def app():
+
+    # Uploading data
+    df = st.file_uploader("Upload a Dataset", type=["csv", "txt"], encoding = "ISO-8859-1")
+    space()
+    if df is not None:
+        # Reading data
+        data = pd.read_csv(df, encoding = "ISO-8859-1")
+        st.dataframe(data.head())
+        space()
+
+        text = st.selectbox("Select Text Column", data.columns)
+        space()
+        target = st.selectbox("Select Target Column", data.columns)
+        space()
+        # print(type(text), type(target))
 
 
-if __name__ == '__main__':
-	main()
+        # Reassigning feature to DataFrame
+        data = data[[text, target]]
+
+        # Droping NaN values
+        data = data.dropna()
+
+        # Initialising class "NLPNaive"
+        nlp_model = NLPNaive(data)
+        
+        # Displaying final DataFrame
+        st.markdown("<h4 style='color: #438a5e'>Final Dataset</h4>", unsafe_allow_html=True)
+        st.dataframe(data.head())
+        space()
 
 
+        # Calling functions for Preprocessing, Bag of Words, Target variable
+        preprocessor = preprocess()
+        space()
+        space()
+        max_features, ngram_range = hyperparameter()
+        space()
+        space()
+        bag_of_words = boW()
+        space()
+        space()
+        y_option = y_label()
+        space()
+        space()
+
+
+        # functions
+        def metrix(corpus, bag_of_words, max_features, ngram_range):
+            try:
+                if bag_of_words == "count_vectorizing":
+                    X = nlp_model.count_vectorizing(corpus, int(max_features), (int(ngram_range[0]),int(ngram_range[1])))
+                    return X
+
+                elif bag_of_words == "tfidf":
+                    X = nlp_model.tf_idf(corpus, int(max_features), (int(ngram_range[0]),int(ngram_range[1])))
+                    return X
+
+            except Exception as e:
+                print("metrix Error : ", e)
+
+
+        def target_series(y_option, target):
+            try:
+                if y_option == "Yes":
+                    y = nlp_model.y_encoding(target)
+                    return y
+
+                elif y_option == "No":
+                    y = data[target]
+                    return y
+            
+            except Exception as e:
+                print("target_series ERROR : ", e)
+
+        # Plotting functions
+        def plot_wordcloud(corpus, y_test, y_pred):
+            st.success("Word Cloud")
+            wordcloud = nlp_model.word_cloud(corpus)
+            st.image(wordcloud)
+            accuracy, cm = nlp_model.cm_accuracy(y_test, y_pred)
+            st.success(f"Accuracy : {round(accuracy*100, 2)}%")
+            st.image(cm)
+
+
+
+
+
+        
+
+        # sentiments
+        def sentimental(text):
+            '''Plotting Sentiments'''
+
+            data['sentiments'] = data[text].apply(nlp_model.sentimental_analysis_clean)
+
+            # Sentiments
+            def getSubjectivity(text):
+                return TextBlob(text).sentiment.subjectivity
+
+            # Create a function to get the polarity
+            def getPolarity(text):
+                return  TextBlob(text).sentiment.polarity
+
+            def getAnalysis(score):
+                if score < 0:
+                    return 'Negative'
+                elif score == 0:
+                    return 'Neutral'
+                else:
+                    return 'Positive'
+
+
+            # Create two new columns 'Subjectivity' & 'Polarity'
+            data['Subjectivity'] = data['sentiments'].apply(getSubjectivity)
+            data['Polarity'] = data['sentiments'].apply(getPolarity)
+            data['Analysis'] = data['Polarity'].apply(getAnalysis)
+
+            st.success("Sentiments")
+
+            sns.countplot(x=data["Analysis"],data=data)
+            st.pyplot(use_container_width=True)
+
+
+            
+                
+
+        # Model Creation
+        if st.button("Analyze"):
+            space()
+            if preprocessor == "Stemming":
+                corpus = nlp_model.stemming(text)
+                X = metrix(corpus, bag_of_words, max_features, ngram_range)
+                y = target_series(y_option, target)
+                X_train, X_test, y_train, y_test = nlp_model.split_data(X, y)
+                y_pred = nlp_model.naive_model(X_train, X_test, y_train, y_test)
+                sentimental(text)
+                plot_wordcloud(corpus, y_test, y_pred)
+                
+
+
+
+
+            elif preprocessor == "Lemmatizing":
+                corpus = nlp_model.lemmatizing(text)
+                X = metrix(corpus, bag_of_words, max_features, ngram_range)
+                y = target_series(y_option, target)
+                X_train, X_test, y_train, y_test = nlp_model.split_data(X, y)
+                y_pred = nlp_model.naive_model(X_train, X_test, y_train, y_test)
+                sentimental(text)
+                plot_wordcloud(corpus, y_test, y_pred)
+                
+
+
+    st.markdown("<h4 style='text-align: center; color: #3f3f44'>©2020 Amar Mandal</h4>", unsafe_allow_html=True)
+
+        
+
+ 
+if __name__ == "__main__":
+    app()
